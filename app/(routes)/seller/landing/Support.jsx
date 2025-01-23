b2a4f4c687714430e57efc18b79e53688a2da640
Tony Ligogo <tonyligogo@gmail.com>
2025-01-23 17:24:56 +0300

    changes

--- app/(routes)/seller/landing/Support.jsx
+++ app/(routes)/seller/landing/Support.jsx
@@ -13,7 +13,7 @@ function Support() {
       </div>
       <h1 className="text-xl mb-3 md:text-4xl text-center font-bold">Supporting your growth every step of the way</h1>
       <p className="text-[17px] md:text-xl text-center">Get the support you need, anytime you need it from a human with 24/7 real-time chat. Our professional support team has your back every step of the way.</p>
-      <div className="flex justify-center gap-3 md:gap-8 mt-8">
+      <div className="flex flex-col md:flex-row justify-center gap-3 md:gap-8 mt-8">
         <div className="flex gap-2 items-center ">
             <span className="bg-primary text-white rounded-md size-12 grid place-content-center"><Clock1 /></span>
             <span className="text-xl">24/7 Live Chat Support</span>
