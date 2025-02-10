759039a15b5e1ba2adbe6ad11ed9d61cc3b76d84
dalienst <oduordalienst@gmail.com>
2025-02-10 11:49:50 +0300

    added different dashboards

--- app/customer/dashboard/page.jsx
+++ app/customer/dashboard/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+import { useFetchUser } from "@/hooks/accounts/actions";
+import { signOut } from "next-auth/react";
+import React from "react";
 
 function Dashboard() {
+  const {
+    isLoading: isLoadingAccount,
+    data: account,
+    refetch: refetchAccount,
+  } = useFetchUser();
+
+  console.log(account);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h1>Hello Customer {account?.first_name}</h1>
+      <button
+        onClick={() => signOut()}
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
